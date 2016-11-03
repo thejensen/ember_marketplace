43a5b87175626f500b1b1d49e5f27626e6f58bde
laura krouse and Sara Jensen <sara@>
2016-11-03 10:18:34 -0700

    add service functionality

--- app/routes/store.js
+++ app/routes/store.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
+    // the datastore is a local copy of data from the database.
     return this.store.findAll('store');
   }
 });
--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -2,11 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   items: [],
+  total: 0,
 
   add(item) {
     this.get('items').pushObject(item);
   },
-  total(items) {
-    
+  addTotal(item) {
+    // .data is a jquery method that retrieves the value of the object's (item) property (cost) in the datastore.
+    console.log(item.data.cost);
+    // variable total uses .get function to retrieve the value of the total property of this service object.
+    var total = this.get('total');
+    // total assigns the value of the item's cost to the total, iterating and adding each item's total in the cart.
+    total += item.data.cost;
+    // this.set sets a property, and takes two arguments. The first argument is 'total', which represents the property named total in this object, and total which is the variable that has been assigned the value of the item's costs above.
+    this.set('total', total);
   }
 });
